82a3bdf24d7e23901469f2e39bfd616d2fec56e2
Péter Molnár <xodax9898@gmail.com>
2024-10-19 16:48:08 +0200

    feat: implementeed and used clerk's authprovider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <p className="text-red-500 font-bold">My thesis</p> 
+    <>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/"/>
+    </div> 
+    </> 
   );
 }

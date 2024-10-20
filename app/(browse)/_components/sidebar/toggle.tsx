cc0b4418e3c753b9b7c7094fe3804b682f1bf19b
Péter Molnár <xodax9898@gmail.com>
2024-10-20 20:52:30 +0200

    feat: tooltip for the togglebutton

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { useSideBar } from "@/store/use-sidebar";
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
+import { Hint } from "@/components/hint";
 
 const Toggle = () => {
     const {
@@ -19,19 +20,23 @@ const Toggle = () => {
         <>
         {collapsed && (
             <div className="hidden lg:flex w-full items-center justify-center pt-4 mb-4">
-                <Button variant="ghost" className="h-auto p-2" onClick={onExpand}>
-                    <ArrowRightFromLine className="h-4 w-4"/>
-                </Button>
+                <Hint label={label} side="right" asChild>
+                    <Button variant="ghost" className="h-auto p-2" onClick={onExpand}>
+                        <ArrowRightFromLine className="h-4 w-4"/>
+                    </Button>
+                </Hint>                
             </div>
         )}
         {!collapsed && (
             <div className="p-3 pl-6 mb-2 flex items-center w-full">
                 <p className="font-semibold text-primary">For you</p>
-                <Button onClick={onCollapse}
-                    className="h-auto p-2 ml-auto"
-                    variant="ghost">
-                    <ArrowLeftFromLine className="h-4 w-4"/> 
-                </Button>
+                <Hint label={label} side="right" asChild>
+                    <Button onClick={onCollapse}
+                        className="h-auto p-2 ml-auto"
+                        variant="ghost">
+                        <ArrowLeftFromLine className="h-4 w-4"/> 
+                    </Button>
+                </Hint>
             </div>
         )}
         </>

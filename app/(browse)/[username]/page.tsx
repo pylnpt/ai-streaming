c0c49b98f0c62d0e41cd4610e68a44c726d15250
Péter Molnár <xodax9898@gmail.com>
2024-10-23 13:20:42 +0200

    feat: finished the follow service Conditional text rendering for the followbutton Appropriate eventhadlers for each case Multiple queries for the backend side of the service

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,13 +1,32 @@
+import { isFollowingUser } from "@/lib/follow-service";
+import { getUserByUserName } from "@/lib/user-service";
+import { notFound } from "next/navigation";
+import { Actions } from "./_components/actions";
+
 interface UserPageProps {
     params: {
         username: string;
     }
 }
 
-const UserPage = ({
+const UserPage = async ({
     params
 }: UserPageProps) => {
-    return ( <div>User: {params.username}</div> );
+    const user = await getUserByUserName(params.username);
+    if(!user) { throw notFound() }
+
+    const isFollowing = await isFollowingUser(user.id);
+    
+    return ( 
+        <div className="flex flex-col gap-y-4">
+            <h1>USER: </h1>
+            <p>Id: {user.id}</p>
+            <p>Username: {user.username}</p>
+            <p>Follower: {`${isFollowing}`}</p>
+            <Actions isFollowing={isFollowing}
+                userId={user.id}/>
+
+        </div> );
 }
  
 export default UserPage;
--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -1,5 +1,6 @@
 import { db } from "./db";
 import { getSelf } from "./authservice";
+import { getUserById } from "./user-service";
 
 export const isFollowingUser = async (id: string) => {   
     try {
@@ -22,4 +23,64 @@ export const isFollowingUser = async (id: string) => {
     } catch {
         return false;   
     }
+}
+export const checkExistingFollow = async (selfId: string, otherUserId: string ) =>{
+    console.log(selfId);
+    console.log(otherUserId)
+    
+    const existingFollow = await db.follow.findFirst({
+        where: {
+            followerId: selfId,
+            followingId: otherUserId
+        },
+    });
+
+    console.log(otherUserId)
+    return existingFollow;
+}
+
+export const followUser = async (id: string) => {
+    const self = await getSelf();
+    const otherUser = await getUserById(id);
+
+    if(!otherUser) { throw new Error("User not found"); }
+    if(otherUser.id === self.id) {throw new Error("Cannot follow yourself"); }
+
+    const existingFollow = await checkExistingFollow(self.id, otherUser.id);
+
+    if(existingFollow) {throw new Error("Already following")};
+
+    const follow = await db.follow.create({
+        data: {
+            followerId: self.id,
+            followingId: otherUser.id,
+        },
+        include: {
+            following: true,
+            follower: true,
+        },
+    });
+    return follow;
+}
+
+export const unfollowUser = async (id: string) => {
+    const self = await getSelf();
+    const otherUser = await getUserById(id);
+    
+    if(!otherUser) { throw new Error("User not found"); }
+    if (otherUser.id === self.id ) {throw new Error("You can't unfollow yourself")}
+
+    const existingFollow = await checkExistingFollow(self.id, otherUser.id);
+    if(!existingFollow) { throw new Error("You are not following this user")}
+    
+    const follow = db.follow.delete({
+        where: {
+            id: existingFollow.id
+        },
+        include: {
+            following: true,
+        }
+    })
+
+    return follow;
 }

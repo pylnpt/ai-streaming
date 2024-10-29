e3753b112740e2cb9c7b4e1a68ef89e930242600
Péter Molnár <xodax9898@gmail.com>
2024-10-29 22:02:41 +0100

    feat!: uploadthing file upload service implementation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
-const config: Config = {
+module.exports = withUt({
     darkMode: ["class"],
     content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	"./src/**/*.{ts,tsx,mdx}"
   ],
   theme: {
   	extend: {
@@ -59,5 +61,4 @@ const config: Config = {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-};
-export default config;
+});

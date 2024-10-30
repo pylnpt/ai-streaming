9c857f2b6fbf979a41c935425081aa857cfbb953
Péter Molnár <xodax9898@gmail.com>
2024-10-30 21:23:48 +0100

    update!: block server action - updated the function so now we can block (temporarily) not logged in users ("guests") too

--- actions/block.ts
+++ actions/block.ts
@@ -1,16 +1,26 @@
 "use server"
 
+import { getSelf } from "@/lib/authservice";
 import { blockUser, unblockUser } from "@/lib/block-service"
+import { RoomServiceClient } from "livekit-server-sdk";
 import { revalidatePath } from "next/cache";
 
+const roomservice = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_SECRET!
+);
+
 export const onBlock = async (id: string) => {
-    const blockedUser = await blockUser(id);
-    
-    revalidatePath("/");
-        
-    if(blockedUser) {
-        revalidatePath(`/${blockedUser.blocked.username}`);
-    }
+    const self = await getSelf();
+    let blockedUser;
+    try { blockedUser = await await blockUser(id); }
+    catch { }
+
+    try { await roomservice.removeParticipant(self.id, id) } 
+    catch { }
+
+    revalidatePath(`/u/${self.username}/community`);
     return blockedUser;
 }
 

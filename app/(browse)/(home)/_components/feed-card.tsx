d7fbea805375f21cbb5a746c6507ce0ecd9e7326
Péter Molnár <xodax9898@gmail.com>
2024-10-31 19:55:23 +0100

    feat: feed-card and feeds components

---
+++ app/(browse)/(home)/_components/feed-card.tsx
@@ -0,0 +1,57 @@
+import { getStreams } from "@/lib/feed-service"
+import { User, Stream } from "@prisma/client";
+import Link from "next/link";
+import { Thumbnail, ThumbNailSkeleton } from "../../_components/thumbnail";
+import { LiveBadge } from "@/components/ui/live-badge";
+import { UserAvartarSkeleton, UserAvatar } from "@/components/user-avatar";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface FeedCardProps {
+    data: Stream & { user: User }
+} 
+
+export const FeedCard = async ({
+    data
+}: FeedCardProps) => {
+    return (
+        <Link href={`/${data.user.username}`}>
+            <div className="h-full w-full space-y-4">
+                <Thumbnail 
+                    src={data.thumbnailUrl}
+                    fallback={data.user.imageUrl}
+                    isStreaming={data.isStreaming}
+                    username={data.user.username}
+                    />
+                    {data.isStreaming && (
+                        <div className="absolute top-2 left-2 group-hover: translate-x-2 group-hover:-translate-y-2 transition-transform">
+                            <LiveBadge/>
+                        </div>
+                    )}
+                    <div className="flex-gap-x-3">
+                        <UserAvatar username={data.user.username}
+                            imageUrl={data.user.imageUrl}
+                            isStreaming={data.isStreaming}/>
+                    </div>
+                    <div className="flex flex-col text-sm overflow-hidden">
+                        <p className="truncate font semibold hover:text-primary">{data.name}</p>
+                        <p className="text-muted-foreground">{data.user.username}</p>
+                    </div>
+            </div>
+        </Link>
+    )
+}
+
+export const FeedCardSkeleton = () => {
+        return (
+            <div className="h-full w-full space-y-4">
+                <ThumbNailSkeleton />
+                <div className="flex gap-x-3">
+                    <UserAvartarSkeleton />
+                    <div className="flex flex-col gap-y-1">
+                        <Skeleton className="h-4 w-32"/>
+                        <Skeleton className="h-3 w-24"/>
+                    </div>
+                </div>
+            </div>
+        )   
+}
---
+++ app/(browse)/(home)/_components/feeds.tsx
@@ -0,0 +1,40 @@
+import { getStreams } from "@/lib/feed-service"
+import { FeedCard, FeedCardSkeleton } from "./feed-card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export const Feeds = async () => {
+    const data = await getStreams();
+    
+    return (
+        <div>
+            <h2 className="text-lg font-semibold mb-4">Currently streaming Users</h2>
+            { data.length === 0 && (
+                <div className="text-muted-foreground text-sm">
+                    There is no one there but crickets. Be the one who changes that.
+                </div>
+            )}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+                {data.map((stream) => (
+                    <div key={stream.id}>
+                        <FeedCard 
+                            key={stream.id}
+                            data={stream} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export const FeedsSkeleton = () => {
+    return (
+        <div>
+            <Skeleton className="h-8 w-[290px] mb-4"/>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+                {[...Array(20)].map((_, i) => (
+                    <FeedCardSkeleton key={i} />
+                ))}
+            </div>
+        </div>
+    )   
+}

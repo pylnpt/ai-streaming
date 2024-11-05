d061de683ed84bdd64cdff91df223222a94c7be4
Péter Molnár <xodax9898@gmail.com>
2024-11-05 17:18:01 +0100

    remove: removed unused imports

--- app/(dashboard)/u/[username]/eyetracking/_components/calibration-modal.tsx
+++ app/(dashboard)/u/[username]/eyetracking/_components/calibration-modal.tsx
@@ -2,29 +2,13 @@
 
 import {
     Dialog,
-    DialogClose,
     DialogContent,
     DialogHeader,
     DialogTitle,
     DialogTrigger
 } from "@/components/ui/dialog"
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue
-} from "@/components/ui/select"
-import {
-    Alert,
-    AlertDescription,
-    AlertTitle,
-} from "@/components/ui/alert"
 
 import { Button } from "@/components/ui/button"
-import { AlertTriangle } from "lucide-react"
-import { useState, useTransition, useRef, ElementRef } from "react"
-import { toast } from "sonner" 
 
 
 
